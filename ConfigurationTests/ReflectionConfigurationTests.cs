df04e0627327030230b21c18248fd294746dcc5d
merc <merc@suigin.local>
2018-09-04 19:36:38 +0200

    Unittests hinzugefügt

---
+++ ConfigurationTests/ReflectionConfigurationTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Configuration;
+using Xunit;
+
+namespace ConfigurationTests
+{
+    public class ReflectionConfigurationTests
+    {
+        [Fact]
+        public void LoadTest()
+        {
+            var configuration = new ReflectionConfiguration("ConfigKram.config");
+            var test = new TestObject{TestString = "", TestInt = 0, TestNullableInt = 1};
+            configuration.Load(test);
+            Assert.Equal("default", test.TestString);
+            Assert.Equal(10, test.TestInt);
+            Assert.Equal(null, test.TestNullableInt);
+        }
+    }
+
+    public class TestObject
+    {
+        public string TestString { get; set; }
+        public int TestInt { get; set; }
+        public int? TestNullableInt { get; set; }
+    }
+}
