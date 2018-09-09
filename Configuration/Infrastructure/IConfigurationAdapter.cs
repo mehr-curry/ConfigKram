8ed2e0b2a9dcb851b1df813240657096e8a7dbc7
merc <merc@suigin.local>
2018-09-09 12:26:58 +0200

    rough refactoring for separating adapter and storage

--- Configuration/Infrastructure/IConfigurationAdapter.cs
+++ Configuration/Infrastructure/IConfigurationAdapter.cs
@@ -1,6 +1,6 @@
 ﻿namespace Configuration
 {
-    public interface IConfiguration
+    public interface IConfigurationAdapter
     {
         void Load(object configurationObject);
         void Save(object configurationObject);
---
+++ Configuration/Infrastructure/IConfigurationStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Configuration
+{
+    public interface IConfigurationStore
+    {
+        IDictionary<string, object> GetValues(string name);
+        void SetValues(string name, IDictionary<string, object> values);
+    }
+}
